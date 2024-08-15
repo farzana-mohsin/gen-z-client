57ac659963aecb398baffe6587a40f90e61b493c
Farzana Mohsin <fmohsin@usertesting.com>
2024-08-15 16:32:21 -0400

    make price in descending order functional

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useLoaderData } from "react-router-dom";
 import Navbar from "./Components/Navbar";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import SingleProduct from "./Components/SingleProduct";
 
 function App() {
   const loader = useLoaderData();
   const [allProducts, setAllProducts] = useState(loader);
 
+  // search
   const handleSearch = (e) => {
     e.preventDefault();
     const form = e.target;
@@ -24,6 +25,14 @@ function App() {
       .then((data) => {
         setAllProducts(data);
       });
+  };
+
+  const handleDescendPrice = () => {
+    fetch(`${import.meta.env.VITE_API_URL}/products-by-price-descend`, {
+      // credentials: "include",
+    })
+      .then((res) => res.json())
+      .then((data) => setAllProducts(data));
   };
 
   return (
@@ -44,10 +53,10 @@ function App() {
               className='dropdown-content menu bg-base-100 rounded-box z-[1] w-64 p-2 shadow'
             >
               <li>
-                <a>Price (Low to High)</a>
+                <a>Price: Low to High:</a>
               </li>
               <li>
-                <a>Price (High to Low)</a>
+                <a onClick={handleDescendPrice}>Price: High to Low</a>
               </li>
               <li>
                 <a>Date Added (Newest first)</a>
@@ -72,6 +81,91 @@ function App() {
               </button>
             </div>
           </form>
+        </div>
+
+        <div className='flex justify-center items-center'>
+          <form
+            onSubmit={""}
+            className='flex my-10'
+          >
+            <div className='join'>
+              <input
+                className='input input-bordered join-item rounded-l-xl border text-sm border-[#6faf9f]'
+                name='input'
+                placeholder='Search here'
+              />
+              <button
+                type='submit'
+                className='btn join-item rounded-r-xl bg-[#6faf9f] text-white  border text-sm hover:bg-[#727C82] border-[#f77d5c]'
+              >
+                Min Price
+              </button>
+            </div>
+          </form>
+          <form
+            onSubmit={""}
+            className='flex my-10'
+          >
+            <div className='join'>
+              <input
+                className='input input-bordered join-item rounded-l-xl border text-sm border-[#6faf9f]'
+                name='input'
+                placeholder='Search here'
+              />
+              <button
+                type='submit'
+                className='btn join-item rounded-r-xl bg-[#6faf9f] text-white  border text-sm hover:bg-[#727C82] border-[#f77d5c]'
+              >
+                Max Price
+              </button>
+            </div>
+          </form>
+          <div className='dropdown'>
+            <div
+              tabIndex={0}
+              role='button'
+              className='btn m-1 px-20'
+            >
+              Brand Name
+            </div>
+            <ul
+              tabIndex={0}
+              className='dropdown-content menu bg-base-100 rounded-box z-[1] w-44 p-2 shadow'
+            >
+              <li>
+                <a>Adidas</a>
+              </li>
+              <li>
+                <a>Nike</a>
+              </li>
+              <li>
+                <a>Puma</a>
+              </li>
+            </ul>
+          </div>
+          <div className='dropdown'>
+            <div
+              tabIndex={0}
+              role='button'
+              className='btn m-1 px-20'
+            >
+              Category
+            </div>
+            <ul
+              tabIndex={0}
+              className='dropdown-content menu bg-base-100 rounded-box z-[1] w-44 p-2 shadow'
+            >
+              <li>
+                <a>Classic</a>
+              </li>
+              <li>
+                <a>Lifestyle</a>
+              </li>
+              <li>
+                <a>Running</a>
+              </li>
+            </ul>
+          </div>
         </div>
         <div className=' my-20 grid grid-cols-1 lg:grid-cols-3 gap-12'>
           {allProducts.map((item, index) => (
